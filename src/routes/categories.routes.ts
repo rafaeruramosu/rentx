7791ff12d001fb1751ecdbf4f85d91ce4eda3d49
Rafael <prfssnlfcs@protonmail.com>
2022-07-19 15:08:35 -0300

    feat: inserindo ID com uuid

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,14 +1,15 @@
-import { Router } from 'express';
+import { Request, Response, Router } from 'express';
+import { v4 as uuidv4 } from 'uuid';
 
 const categoriesRoutes = Router();
 
 const categories = [];
 
-categoriesRoutes.post('/categories', (request, response) => {
+categoriesRoutes.post('/', (request: Request, response: Response) => {
   const { name, description } = request.body;
 
   const category = {
-    // id: uuidv4(),
+    id: uuidv4(),
     name,
     description,
     created_at: new Date(),
