cf8c80d723458c8f3c6a30d31085c7a57360b0af
Rafael <prfssnlfcs@protonmail.com>
2022-07-19 14:49:29 -0300

    chore: debugando a aplicacao

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (request, response) => {
+  return response.json({ message: 'hello' });
+});
+
+app.post('/courses', (request, response) => {
+  const { name } = request.body;
+
+  return response.json({ name });
+});
+
+app.listen(3333, () => console.log('server is running'));
