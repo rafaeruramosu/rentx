5c14cb890a1fb6c505533e005ccb3d2e76442132
Rafael <prfssnlfcs@protonmail.com>
2022-07-19 15:25:19 -0300

    feat: inserindo tipagem para categoria

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,23 +1,21 @@
 import { Request, Response, Router } from 'express';
-import { v4 as uuidv4 } from 'uuid';
+
+import { Category } from '../model/Category';
 
 const categoriesRoutes = Router();
 
-const categories = [];
+const categories: Category[] = [];
 
 categoriesRoutes.post('/', (request: Request, response: Response) => {
   const { name, description } = request.body;
 
-  const category = {
-    id: uuidv4(),
-    name,
-    description,
-    created_at: new Date(),
-  };
+  const category = new Category();
+
+  Object.assign(category, { name, description, created_at: new Date() }); // assign recebe um objeto de primeiro parametro e de segundo, os atributos que ele precisará passar para dentro do objeto item a item
 
   categories.push(category);
 
-  return response.status(201).send();
+  return response.status(201).json(category);
 });
 
 export { categoriesRoutes };
