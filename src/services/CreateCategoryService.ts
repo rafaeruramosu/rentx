e6efbe32256fae96bc5a63622c7e4c633521f83c
Rafael <prfssnlfcs@protonmail.com>
2022-07-21 14:54:03 -0300

    feat: utilizando o Principio de Responsabilidade Unica (SRP) *SOLID*

---
+++ src/services/CreateCategoryService.ts
@@ -0,0 +1,22 @@
+import { CategoriesRepository } from '../repositories/CategoriesRepository';
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
+class CreateCategoryService {
+  constructor(private categoriesRepository: CategoriesRepository) {} // D => DIP - Dependency Inversion Principle (Princípio da Inversão de Dependência) *SOLID*
+
+  execute({ name, description }: IRequest): void {
+    const categoryAlreadyExists = this.categoriesRepository.findByName(name);
+
+    if (categoryAlreadyExists) {
+      throw new Error('Category already exists.');
+    }
+
+    this.categoriesRepository.create({ name, description });
+  }
+}
+
+export { CreateCategoryService };
