c40bb26e425ee5705cb3c9103832976d9d426dff
Rafael <prfssnlfcs@protonmail.com>
2022-07-21 15:49:55 -0300

    feat: criando repositorio de especificao

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 
 import { categoriesRoutes } from './routes/categories.routes';
+import { specificationsRoutes } from './routes/specifications.routes';
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/categories', categoriesRoutes); // todas as rotas terão no começo delas o path /categories
+app.use('/specifications', specificationsRoutes);
 
 app.listen(3333, () => console.log('server is running'));
