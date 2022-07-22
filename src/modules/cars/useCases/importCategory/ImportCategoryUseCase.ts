d13f10fb65fd726e728a0cc84d0b06ed4c49dc6e
Rafael <prfssnlfcs@protonmail.com>
2022-07-22 15:35:36 -0300

    feat: conhecendo o conceito de stream

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,6 +1,17 @@
+import { parse as csvParse } from 'csv-parse';
+import fs from 'fs';
+
 class ImportCategoryUserCase {
-  execute(file: any) {
-    console.log(file);
+  execute(file: Express.Multer.File): void {
+    const stream = fs.createReadStream(file.path);
+
+    const parseFile = csvParse();
+
+    stream.pipe(parseFile); // 'pipe' pega o que está sendo lido do stream e joga para algum lugar determinado como parâmetro dele
+
+    parseFile.on('data', async line => {
+      console.log(line);
+    });
   }
 }
 
