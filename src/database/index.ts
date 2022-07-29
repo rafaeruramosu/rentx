8f594a064e75550a524bd68463abf14bc9e74864
Rafael <prfssnlfcs@protonmail.com>
2022-07-29 16:04:21 -0300

    feat: criando migration de usuario

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,9 +1,11 @@
 import { DataSource } from 'typeorm';
 
+import { User } from '../modules/accounts/entities/User';
 import { Category } from '../modules/cars/entities/Category';
 import { Specification } from '../modules/cars/entities/Specification';
 import { CreateCategories1658862631345 } from './migrations/1658862631345-CreateCategories';
 import { CreateSpecifications1659117982083 } from './migrations/1659117982083-CreateSpecifications';
+import { CreateUsers1659120825949 } from './migrations/1659120825949-CreateUsers';
 
 const dataSource = new DataSource({
   type: 'postgres',
@@ -11,10 +13,11 @@ const dataSource = new DataSource({
   username: 'docker',
   password: 'ignite',
   database: 'rentx',
-  entities: [Category, Specification],
+  entities: [Category, Specification, User],
   migrations: [
     CreateCategories1658862631345,
     CreateSpecifications1659117982083,
+    CreateUsers1659120825949,
   ],
 });
 
