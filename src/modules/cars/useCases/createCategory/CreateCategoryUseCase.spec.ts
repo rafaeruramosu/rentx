3d361dd2d59bfc95372a37cbd1f4836282a694de
Rafael <prfssnlfcs@protonmail.com>
2022-08-04 13:45:11 -0300

    test: criando o primeiro teste

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -0,0 +1,9 @@
+describe('Create category', () => {
+  it('should be able to create a category', () => {
+    const soma = 2 + 1;
+
+    const result = 4;
+
+    expect(soma).toBe(result);
+  });
+});
