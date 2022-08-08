c9b2af0e1c59a37dec5414380beb9d705b3416a7
Rafael <prfssnlfcs@protonmail.com>
2022-08-08 17:45:05 -0300

    feat: estruturando a entidade de carros

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 
 import { authenticateRoutes } from './authenticate.routes';
+import { carsRoutes } from './cars.routes';
 import { categoriesRoutes } from './categories.routes';
 import { specificationsRoutes } from './specifications.routes';
 import { usersRoutes } from './users.routes';
@@ -10,6 +11,7 @@ const router = Router();
 router.use('/categories', categoriesRoutes); // dessa forma todas as rotas terão no começo delas o path /categories
 router.use('/specifications', specificationsRoutes); // dessa forma todas as rotas terão no começo delas o path /specifications
 router.use('/users', usersRoutes); // dessa forma todas as rotas terão no começo delas o path /users
+router.use('/cars', carsRoutes); // dessa forma todas as rotas terão no começo delas o path /cars
 router.use(authenticateRoutes); // dessa forma todas as rotas terão no começo delas o path /
 
 export { router };
--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,6 +1,7 @@
 import { DataSource } from 'typeorm';
 
 import { User } from '@modules/accounts/infra/typeorm/entities/User';
+import { Car } from '@modules/cars/infra/typeorm/entities/Car';
 import { Category } from '@modules/cars/infra/typeorm/entities/Category';
 import { Specification } from '@modules/cars/infra/typeorm/entities/Specification';
 
@@ -17,7 +18,7 @@ const dataSource = new DataSource({
   username: 'docker',
   password: 'ignite',
   database: 'rentx',
-  entities: [Category, Specification, User],
+  entities: [Category, Specification, User, Car],
   migrations: [
     CreateCategories1658862631345,
     CreateSpecifications1659117982083,
