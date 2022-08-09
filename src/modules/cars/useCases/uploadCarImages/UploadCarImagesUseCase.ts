52ebf380a7ce6304a51674ce6a35ac84a960ce89
Rafael <prfssnlfcs@protonmail.com>
2022-08-09 15:13:23 -0300

    feat: caso de uso do cadastro de imagens do carro

--- src/modules/cars/useCases/uploadCarImages/UploadCarImagesUseCase.ts
+++ src/modules/cars/useCases/uploadCarImages/UploadCarImagesUseCase.ts
@@ -1,5 +1,24 @@
+import { inject, injectable } from 'tsyringe';
+
+import { ICarImagesRepository } from '@modules/cars/repositories/ICarImagesRepository';
+
+interface IRequest {
+  car_id: string;
+  images_name: string[];
+}
+
+@injectable()
 class UploadCarImagesUseCase {
-  async execute(): Promise<void> {}
+  constructor(
+    @inject('CarImagesRepository')
+    private carImagesRepository: ICarImagesRepository,
+  ) {} // D => DIP - Dependency Inversion Principle (Princípio da Inversão de Dependência) *SOLID*
+
+  async execute({ car_id, images_name }: IRequest): Promise<void> {
+    images_name.map(async image => {
+      await this.carImagesRepository.create(car_id, image);
+    });
+  }
 }
 
 export { UploadCarImagesUseCase };
