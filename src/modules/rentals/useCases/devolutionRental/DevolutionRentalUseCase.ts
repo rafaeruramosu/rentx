a4f09b8f0c243423480ee4df671976c5807f4d12
Rafael <prfssnlfcs@protonmail.com>
2022-08-16 15:50:03 -0300

    feat: controller de devolução de carro

--- src/modules/rentals/useCases/devolutionRental/DevolutionRentalUseCase.ts
+++ src/modules/rentals/useCases/devolutionRental/DevolutionRentalUseCase.ts
@@ -44,8 +44,8 @@ class DevolutionRentalUseCase {
     }
 
     const delay = this.dateProvider.compareInDays(
-      dateNow,
       rental.expected_return_date,
+      dateNow,
     );
 
     let total = 0;
