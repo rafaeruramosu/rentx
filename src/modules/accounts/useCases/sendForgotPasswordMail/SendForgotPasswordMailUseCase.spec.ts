2133b26c4a87bc2dd2a92f4990aa1a5b0e04c071
Rafael <prfssnlfcs@protonmail.com>
2022-08-19 11:15:33 -0300

    test: testando envio de e-mail

---
+++ src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.spec.ts
@@ -0,0 +1,49 @@
+import { UsersRepositoryInMemory } from '@modules/accounts/repositories/in-memory/UsersRepositoryInMemory';
+import { UsersTokensRepositoryInMemory } from '@modules/accounts/repositories/in-memory/UsersTokensRepositoryInMemory';
+import { DayjsDateProvider } from '@shared/container/providers/DateProvider/implementations/DayjsDateProvider';
+import { MailProviderInMemory } from '@shared/container/providers/MailProvider/repositories/in-memory/MailProviderInMemory';
+import { AppError } from '@shared/errors/AppError';
+
+import { SendForgotPasswordMailUseCase } from './SendForgotPasswordMailUseCase';
+
+let usersRepositoryInMemory: UsersRepositoryInMemory;
+let usersTokensRepositoryInMemory: UsersTokensRepositoryInMemory;
+let dayjsDateProvider: DayjsDateProvider;
+let mailProvider: MailProviderInMemory;
+let sendForgotPasswordMailUseCase: SendForgotPasswordMailUseCase;
+
+describe('Send forgot password mail', () => {
+  beforeEach(() => {
+    usersRepositoryInMemory = new UsersRepositoryInMemory();
+    usersTokensRepositoryInMemory = new UsersTokensRepositoryInMemory();
+    dayjsDateProvider = new DayjsDateProvider();
+    mailProvider = new MailProviderInMemory();
+    sendForgotPasswordMailUseCase = new SendForgotPasswordMailUseCase(
+      usersRepositoryInMemory,
+      usersTokensRepositoryInMemory,
+      dayjsDateProvider,
+      mailProvider,
+    );
+  });
+
+  it('should be able to send a forgot password mail to user', async () => {
+    const sendMail = jest.spyOn(mailProvider, 'sendMail');
+
+    await usersRepositoryInMemory.create({
+      driver_license: 'driver_license test',
+      name: 'test',
+      email: 'email@test.com',
+      password: 'password test',
+    });
+
+    await sendForgotPasswordMailUseCase.execute('email@test.com');
+
+    expect(sendMail).toHaveBeenCalled();
+  });
+
+  it('should not be able to send a forgot password mail to nonexistent user', async () => {
+    await expect(
+      sendForgotPasswordMailUseCase.execute('email@test.com'),
+    ).rejects.toEqual(new AppError('User does not exists'));
+  });
+});
