e1aa85fdbff46c74fff34998a2ea03f9ea991fa2
Rafael <prfssnlfcs@protonmail.com>
2022-08-25 15:49:43 -0300

    feat: configurando sentry

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -6,6 +6,8 @@ import 'express-async-errors';
 import swaggerUi from 'swagger-ui-express';
 
 import upload from '@config/upload';
+import * as Sentry from '@sentry/node';
+import * as Tracing from '@sentry/tracing';
 import { AppError } from '@shared/errors/AppError';
 import { router } from '@shared/infra/http/routes';
 import { createConnection } from '@shared/infra/typeorm';
@@ -20,7 +22,24 @@ const app = express();
 
 app.use(rateLimiter);
 
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [
+    new Sentry.Integrations.Http({ tracing: true }),
+    new Tracing.Integrations.Express({ app }),
+  ],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
+app.use(Sentry.Handlers.requestHandler());
+
 app.use(express.json());
+
+app.use(Sentry.Handlers.tracingHandler());
 
 app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
@@ -30,6 +49,8 @@ app.use('/cars', express.static(`${upload.tmpFolder}/cars`));
 app.use(cors());
 
 app.use(router);
+
+app.use(Sentry.Handlers.errorHandler());
 
 app.use(
   (err: Error, request: Request, response: Response, next: NextFunction) => {
